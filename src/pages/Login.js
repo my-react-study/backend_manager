e85eb5582a3949e2f6fa4636e4034f1b129bc8f7
tangmingheng <mhtang@thoughtworks.com>
2020-03-08 22:29:24 +0800

    修改登录页面

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
-import { Button } from 'antd';
+import { Card, Input, Icon, Button, Spin, message } from 'antd';
+import '../static/css/Login.css';
 
 function Login(props) {
-    const login = () => {
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const checkLogin = () => {
+        setIsLoading(true)
+        if (!userName) {
+            message.error('用户名不能为空')
+            setTimeout(() => {
+                setIsLoading(false)
+            }, 500)
+
+            return false
+        } else if (!password) {
+            message.error('密码不能为空')
+            setTimeout(() => {
+                setIsLoading(false)
+            }, 500)
+            return false
+        }
         props.history.push('/index')
     }
-    
+
     return (
-        <div>
-            <Button type="primary" size="large" block onClick={login} > Login in </Button>
+        <div className="login-div">
+
+            <Spin tip="Loading..." spinning={isLoading}>
+                <Card title="用户登录" bordered={true} style={{ width: 400 }} >
+                    <Input
+                        id="userName"
+                        size="large"
+                        placeholder="Enter your userName"
+                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        onChange={(e) => { setUserName(e.target.value) }}
+                    />
+                    <br /><br />
+                    <Input.Password
+                        id="password"
+                        size="large"
+                        placeholder="Enter your password"
+                        prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        onChange={(e) => { setPassword(e.target.value) }}
+                    />
+                    <br /><br />
+                    <Button type="primary" size="large" block onClick={checkLogin} > 登录 </Button>
+                </Card>
+            </Spin>
         </div>
     )
 }
