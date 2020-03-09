990e30dd89eb3b1066bc8500f85d3b10f5d44dcb
tangmingheng <mhtang@thoughtworks.com>
2020-03-09 20:28:33 +0800

    添加博客首页

---
+++ src/pages/BlogIndex.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Divider } from 'antd';
+
+const BlogIndex = () => {
+    return (
+        <div>
+            <h2>我是博客首页</h2>
+        </div>
+    )
+}
+export default BlogIndex
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import Video from "./Video"
 import Workplace from "./Workplace"
+import BlogIndex from "./BlogIndex"
 import "./Home.css"
 
 function Home() {
@@ -10,14 +11,14 @@ function Home() {
             <div className="leftNav">
                 <h3>一级导航</h3>
                 <ul>
-                    {/* <li><Link to="/">博客首页</Link> </li> */}
+                    <li><Link to="/home/index">博客首页</Link> </li>
                     <li><Link to="/home/video">视频教程</Link> </li>
                     <li><Link to="/home/workplace">职场空间</Link> </li>
                 </ul>
             </div>
 
             <div className="rightMain">
-                {/* <Route path="/" exact component={Index} /> */}
+                <Route path="/home/index" exact component={BlogIndex} />
                 <Route path="/home/video" component={Video} />
                 <Route path="/home/workplace" component={Workplace} />
             </div>
