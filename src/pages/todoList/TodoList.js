8edc471698cea1ddd325a6e48898118577bc6afe
tangmingheng <mhtang@thoughtworks.com>
2020-03-21 19:57:11 +0800

    添加编辑用户弹窗实现

--- src/pages/todoList/TodoList.js
+++ src/pages/todoList/TodoList.js
@@ -1,43 +1,11 @@
 import React, { Component } from 'react';
-import { Table, Input, Row, Button } from 'antd';
+import { Table, Input, Row, Button, Modal, Form, message } from 'antd';
 import 'antd/dist/antd.css'
 import store from './store/storeCreator'
-import { getUserListAction, getAllUserAction, getUsersByUsername } from "./store/actionCreator";
+import { getAllUserAction, getUsersByUsername } from "./store/actionCreator";
 
 const { Search } = Input;
-
-const userList = [
-    {
-        username: "tom",
-        age: "18",
-        address: "武汉",
-    },
-    {
-        username: "tom",
-        age: "18",
-        address: "武汉",
-    },
-    {
-        username: "tom",
-        age: "18",
-        address: "武汉",
-    },
-    {
-        username: "tom",
-        age: "18",
-        address: "武汉",
-    },
-    {
-        username: "tom",
-        age: "18",
-        address: "武汉",
-    },
-    {
-        username: "tom",
-        age: "18",
-        address: "武汉",
-    }
-]
+const FormItem = Form.Item;
 
 class TodoList extends Component {
 
@@ -62,17 +30,13 @@ class TodoList extends Component {
         {
             dataIndex: "action", title: "操作", width: 200, render: (text, row) => {
                 return <div>
-                    <Button >编辑</Button>
+                    <Button onClick={() => this.modal('edit', row)}>编辑</Button>
                     <Button style={{ marginLeft: 10 }} type="danger" >删除</Button>
                 </div>
             }
         }];
 
     componentDidMount() {
-        // axios.get('http://localhost:7300/mock/5e74ab0eca458a00267bf30a/tmh/getUserList').then((res) => {
-        //     const userList = res.data.userList
-        //     const action = getUserListAction(userList)
-        //     store.dispatch(action)
         const action = getAllUserAction()
         store.dispatch(action)
     }
@@ -86,19 +50,76 @@ class TodoList extends Component {
         store.dispatch(action)
     };
 
+    //添加编辑用户弹窗
+    modal = (type, row) => {
+        this.setState({
+            visible: true,
+            modalType: type
+        }, () => {
+            this.props.form.resetFields();
+            if (type === 'add') return;
+            this.props.form.setFieldsValue({
+                username: row.username,
+                age: row.age,
+                address: row.address
+            })
+            this.setState({ editRow: row })
+        })
+    }
+
     render() {
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 4 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+
         return (
             <div className="App">
                 <Row>
                     <Search style={{ width: 300 }} onChange={this.search} placeholder="请输入姓名" />
-                    <Button type="primary" style={{ marginLeft: 20 }} >添加用户</Button>
+                    <Button type="primary" style={{ marginLeft: 20 }} onClick={() => this.modal('add')}>添加用户</Button>
                 </Row>
                 <Row style={{ paddingTop: 20 }}>
                     <Table dataSource={this.state.userList} rowKey={row => row.id} bordered columns={this.columns} />
                 </Row>
+                <Modal
+                    title={this.state.modalType === 'add' ? "添加用户" : "编辑用户"}
+                    onCancel={() => this.setState({ visible: false })}
+                    visible={this.state.visible}>
+                    <Form>
+                        <FormItem label="用户"  {...formItemLayout}>
+                            {getFieldDecorator('username', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input placeholder="username" />
+                            )}
+                        </FormItem>
+                        <FormItem label="年龄"  {...formItemLayout}>
+                            {getFieldDecorator('age', {
+                                rules: [{ required: true, message: 'Please input your age!' }],
+                            })(
+                                <Input placeholder="age" />
+                            )}
+                        </FormItem>
+                        <FormItem label="地址"  {...formItemLayout}>
+                            {getFieldDecorator('address', {
+                                rules: [{ required: true, message: 'Please input your address!' }],
+                            })(
+                                <Input placeholder="address" />
+                            )}
+                        </FormItem>
+                    </Form>
+                </Modal>
             </div>
         );
     }
 }
 
-export default TodoList;
+export default Form.create()(TodoList);
